512dc1699789f26bb63b07ee7e5626b25db4c4e3
Joshua Cachola <joshuaelliscachola@gmail.com>
2022-03-16 17:26:37 -0700

    add: initial setup, working on header/navbar

---
+++ src/lib/header/Header.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+</script>
+
+<header>
+  <nav>
+    <ul class="flex-1 justify-center list-none">
+      <li>Home</li>
+      <li>About</li>
+      <li>Projects</li>
+      <li>Contact</li>
+    </ul>
+  </nav>
+</header>
+
