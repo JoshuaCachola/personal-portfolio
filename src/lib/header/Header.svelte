4fe25d66954782aaf2d0c0ca6569c4e35394e148
Joshua Cachola <joshuaelliscachola@gmail.com>
2022-03-18 16:54:33 -0700

    add: images for about and projects background and start on projects view

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -17,12 +17,12 @@
 		<!-- Desktop View -->
 		<MediaQuery query={DESKTOP} let:matches>
 			{#if matches}
-      <div class="side-menu absolute right-0 translate-x-28 top-0 translate-y-32">
+      <div class="side-menu fixed top-0 right-0 translate-y-32 translate-x-28 z-10">
 				<ul class="list-none rotate-90 flex text-lg text-white" style="font-family: 'Assistant', sans-serif;">
-					<li class="ml-4 hover:border-b-2 hover:font-semibold border-amber-100">Home</li>
-					<li class="ml-4 hover:border-b-2 hover:font-semibold border-amber-100">About</li>
-					<li class="ml-4 hover:border-b-2 hover:font-semibold border-amber-100">Projects</li>
-					<li class="ml-4 hover:border-b-2 hover:font-semibold border-amber-100">Contact</li>
+					<li class="mr-4 hover:border-b-2 hover:font-semibold border-amber-100">Home</li>
+					<li class="mr-4 hover:border-b-2 hover:font-semibold border-amber-100">About</li>
+					<li class="mr-4 hover:border-b-2 hover:font-semibold border-amber-100">Projects</li>
+					<li class="mr-4 hover:border-b-2 hover:font-semibold border-amber-100">Contact</li>
 				</ul>
       </div>
 			{/if}
@@ -32,7 +32,7 @@
 		<MediaQuery query={MOBILE} let:matches>
 			{#if matches}
 				<div
-					class="cursor-pointer w-2/3 h-10 fixed bottom-8 translate-x-1/4 bg-slate-700"
+					class="cursor-pointer w-2/3 h-10 fixed bottom-8 translate-x-1/4 bg-slate-700 z-10"
 					on:click={handleShowMobileNav}
 				>
 					<div class="relative flex justify-center">
--- src/routes/projects.svelte
+++ src/routes/projects.svelte
@@ -1,4 +1,40 @@
 <script lang="ts">
-  
+  import background from "$lib/images/projects-bg.jpg";
+  import background2 from "$lib/images/background2.png";
+
+  const projects: {name: string, description: string}[] = [{
+      name: "SkateSpot", 
+      description: "Crowd-sourced social platform for skaters."}
+    // }, {
+    //   name: "Splitsmart",
+    //   description: "A social and easy way to split the bill."
+    // }, {
+    //   name: "Prova",
+    //   description: "An easy way to track your runs."
+    // }, {
+    //   name: "Thirteen",
+    //   description: "A Southeastern Asian card game played with 2-4 players."
+    // }
+  ];
+
 </script>
 
+<section class="flex flex-col justify-center items-center h-screen">
+  <h2 class="freehand text-amber-100 text-5xl z-10">Projects</h2>
+  <div class="w-full h-1/2 bg-cover" style="background-image: url({background});">
+    <div class="h-full assistant opacity-90 text-slate-700" style="background-image: url({background2});">
+      <div class="font-semibold w-11/12 m-auto flex flex-col relative">
+        {#each projects as project}
+          <div class="w-1/2 h-full bg-black absolute top-2 left-2 border-r-[1px] border-b-[1px] border-black"/>
+          <div class="w-1/2 bg-white opacity-100 z-10 border-r-[1px] border-b-[1px] border-black">
+            <h3>{project.name}</h3>
+            <p class="font-extralight">{project.description}</p>
+            <div class="flex space-around">
+
+            </div>
+          </div>
+        {/each}
+      </div>
+    </div>
+  </div>
+</section>
