08fd4769496e49d83fac33f1a5fec42d0c60d0d1
Joshua Cachola <joshuaelliscachola@gmail.com>
2022-03-18 23:25:17 -0700

    add: contact page

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,4 +1,4 @@
-<script context="module">
+<!-- <script context="module">
 	import { browser, dev } from '$app/env';
 
 	// we don't need any JS on this page, though we'll load
@@ -12,12 +12,31 @@
 	// since there's no dynamic data here, we can prerender
 	// it so that it gets served as a static asset in prod
 	export const prerender = true;
+</script> -->
+<script lang="ts">
+  import MediaQuery from "svelte-media-query";
+  import { DESKTOP, MOBILE } from "$lib/constants";
+	import background from "$lib/about-bg.jpg"
 </script>
 
 <svelte:head>
-	<title>Projects</title>
+	<title>About</title>
 </svelte:head>
 
-<div class="content">
-	
-</div>
+<section class="flex justify-evenly items-center h-screen">
+  <!-- Desktop view -->
+  <MediaQuery query={DESKTOP} let:matches>
+    {#if matches}
+			<h2 class="freehand text-amber-100 text-5xl">About</h2>
+			<div class="w-5/6 h-96" style="background-image: url({background});"></div>
+    {/if}
+  </MediaQuery>
+
+  <!-- Mobile view -->
+  <MediaQuery query={MOBILE} let:matches>
+    {#if matches}
+			<h2 class="freehand text-amber-100 text-2xl">About</h2>
+			
+    {/if}
+  </MediaQuery>
+</section>
--- src/routes/contact.svelte
+++ src/routes/contact.svelte
@@ -1,24 +1,57 @@
 <script lang="ts">
-  import MediaQuery from "svelte-media-query";
-  import { DESKTOP, MOBILE } from "$lib/constants";
+  // import MediaQuery from "svelte-media-query";
+  // import { DESKTOP, MOBILE } from "$lib/constants";
+	import background from "$lib/images/projects-bg.jpg"
+  let width: number;
+  $: isDesktop = width > 720 ? true : false;
+  
 </script>
 
-<section>
-  <!-- Desktop view -->
-  <MediaQuery query={DESKTOP} let:matches>
-    {#if matches}
-      <div class="flex flex-col justify-center items-center h-screen">
-        <h2 class="freehand text-amber-100 text-5xl">Contact</h2>
+<!-- <section class="flex flex-col justify-evenly items-center h-screen relative" bind:clientWidth={width}>
+  <h2 class="freehand text-amber-100 {isDesktop ? 'text-5xl' : 'text-2xl'}">Contact</h2>
+  <div class="bg-cover w-1/2 h-1/2 flex justify-center items-center relative" style="background-image: url({background});">
+  <div class="w-full h-full absolute top-2 left-2 bg-black"></div>
+  <div class="w-full">
+    <form method="post" action="https://formspree.io/xayponkz">
+      <div class="flex">
+        <div class="flex">
+          <div>
+            <label for="name" class="assistant text-xl">Name</label>
+            <input type="text" name="name" id="name" class="bg-slate-700 text-white border-b-2 border-black" />
+          </div>
+          <div>
+            <label for="email" class="assistant text-xl">Email</label>
+            <input type="email" name="email" id="email" class="bg-slate-700 text-white" />
+          </div>
+        </div>
+        <div></div>
+          <label for="message" class="assistant text-xl">Message</label>
+          <textarea name="message" id="message" class="bg-slate-700 text-white"></textarea>
       </div>
-    {/if}
-  </MediaQuery>
-
-  <!-- Mobile view -->
-  <MediaQuery query={DESKTOP} let:matches>
-    {#if matches}
-      <div class="flex flex-col justify-center items-center h-screen">
-        <h2 class="freehand text-amber-100 text-2xl">Contact</h2>
+      <ul class="actions">
+        <li><input type="submit" value="Send" class="primary" /></li>
+      </ul>
+    </form>
+    </div>
+  </div>
+  
+</section> -->
+<section class="flex flex-col justify-evenly items-center h-screen relative" bind:clientWidth={width}>
+  <h2 class="freehand text-amber-100 {isDesktop ? 'text-5xl' : 'text-2xl'}">Contact</h2>
+    <form method="post" action="https://formspree.io/xayponkz">
+      <div class="fields">
+        <div class="field half">
+          <input type="text" name="name" id="name" placeholder="Name" />
+        </div>
+        <div class="field half">
+          <input type="email" name="email" id="email" placeholder="Email" />
+        </div>
+        <div class="field">
+          <textarea name="message" id="message" placeholder="Message" class="focus:outline-none"></textarea>
+        </div>
       </div>
-    {/if}
-  </MediaQuery>
+      <div class="flex justify-center">
+        <input type="submit" value="Send" class="text-white cursor-pointer hover:border-b-2 hover:border-amber-100 first-line:hover:text-amber-100" />
+      </div>
+    </form>
 </section>
